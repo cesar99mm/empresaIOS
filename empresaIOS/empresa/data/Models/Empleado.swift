3eb8b3fd507a25c1467f0b14805ba3bf34050918
cesar99mm <98267813+cesar99mm@users.noreply.github.com>
2022-03-30 12:46:00 +0200

    Add files via upload

---
+++ empresaIOS/empresa/data/Models/Empleado.swift
@@ -0,0 +1,17 @@
+//
+//  Empleado.swift
+//  empresa
+//
+//  Created by APPS2T on 24/2/22.
+//
+
+import Foundation
+
+struct Empleado: Codable {
+    var id: Int
+    var nombre: String
+    var biografia: String
+    var puesto: String
+    var salario: Int
+    var url: String
+}
